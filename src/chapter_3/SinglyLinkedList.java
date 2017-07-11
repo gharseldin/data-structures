e8271921783654d53bbb377e7043481ce6a5f203
Amr Gharseldin <gharseldin@aucegypt.edu>
2017-07-10 17:00:39 -0700

    Equal and Clone

--- src/chapter_3/SinglyLinkedList.java
+++ src/chapter_3/SinglyLinkedList.java
@@ -1,6 +1,6 @@
 package chapter_3;
 
-public class SinglyLinkedList <E> {
+public class SinglyLinkedList <E> implements Cloneable{
 
 	// Nested Node class
 	private static class Node<E>{
@@ -74,5 +74,36 @@ public class SinglyLinkedList <E> {
 		}
 		return returnValue;
 		
+	}
+	
+	public boolean equals(Object o){
+		if( o == null)return false;
+		if(getClass() != o.getClass())return false;
+		SinglyLinkedList other = (SinglyLinkedList)o;
+		if(size != other.size())return false;
+		Node walkA = head;
+		Node walkB = other.head;
+		while(walkA !=null){
+			if(!walkA.getElement().equals(walkB.getElement()))return false;
+			walkA = walkA.getNext();
+			walkB = walkB.getNext();
+		}
+		return true;
+	}
+	
+	public SinglyLinkedList<E> clone() throws CloneNotSupportedException {
+		SinglyLinkedList<E> other = (SinglyLinkedList<E>) super.clone();
+		if(size > 0){
+			other.head = new Node<>(head.getElement(), null);
+			Node<E> walk = head.getNext();
+			Node<E> otherTail = other.head;
+			while(walk != null){
+				Node<E> newest = new Node<>(walk.getElement(),null);
+				otherTail.setNext(newest);
+				otherTail = newest;
+				walk = walk.getNext();
+			}
+		}
+		return other;
 	}
 }
