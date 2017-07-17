2a5cb8ab02b9fd9e55323d6d418f6928cfcaa985
gharseldin <gharseldin@aucegypt.edu>
2017-07-16 23:56:59 -0700

    Chapter 4
    starting questions

--- src/chapter_4/BigOhAnalysis.java
+++ src/chapter_4/BigOhAnalysis.java
@@ -101,4 +101,15 @@ public class BigOhAnalysis {
 		return a;
 	}
 	
+	
+	public static int arrayFind(int[] data, int val){
+		int n = data.length;
+		int j = 0;
+		while (j<n){
+			if(data[j] == val)
+				return j;
+			j++;
+		}
+		return -1;
+	}
 }
