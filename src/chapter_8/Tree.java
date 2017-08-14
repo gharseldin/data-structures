33f6613d762224e02a78fdabac8a83941d812ad3
gharseldin <gharseldin@aucegypt.edu>
2017-08-14 00:14:18 -0700

    Chapter 8 Tree ADT interface

---
+++ src/chapter_8/Tree.java
@@ -0,0 +1,20 @@
+package chapter_8;
+
+import java.util.Iterator;
+import chapter_7.Position;
+
+public interface Tree<E> extends Iterable<E>{
+	
+	Position<E> root();
+	Position<E> parent(Position<E> p) throws IllegalArgumentException;
+	Iterable<Position<E>> children(Position<E> p) throws IllegalArgumentException;
+	int numChildren(Position<E> p) throws IllegalArgumentException;
+	boolean isInternal(Position<E> p) throws IllegalArgumentException;
+	boolean isExternal(Position<E> p) throws IllegalArgumentException;
+	boolean isRoot(Position<E> p) throws IllegalArgumentException;
+	int size();
+	boolean isEmpty();
+	Iterator<E> iterator();
+	Iterable<Position<E>> positions();
+	
+}
