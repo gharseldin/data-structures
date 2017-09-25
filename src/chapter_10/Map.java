32acc954e77e0df26b54444c996d82e2fb8c51e2
Amr Gharseldin <gharseldin@aucegypt.edu>
2017-09-24 22:32:25 -0700

    Chapter 10 -Map interface

---
+++ src/chapter_10/Map.java
@@ -0,0 +1,16 @@
+package chapter_10;
+
+import chapter_9.Entry;
+
+public interface Map <K, V>{
+	
+	int size();
+	boolean isEmpty();
+	V get(K key);
+	V put(K key, V value);
+	V remove(K key);
+	Iterable<K> keySet();
+	Iterable<V> values();
+	Iterable<Entry<K,V>> entrySet();
+
+}
