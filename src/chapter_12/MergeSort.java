ca8a9175ed534307cb6088912badee60ed94f61b
Amr Gharseldin <gharseldin@aucegypt.edu>
2017-10-25 21:57:54 -0700

    chapter 12 -Merge sort implementation for a Queue implemented with a Linked List

--- src/chapter_12/MergeSort.java
+++ src/chapter_12/MergeSort.java
@@ -3,6 +3,9 @@ package chapter_12;
 import java.util.Arrays;
 import java.util.Comparator;
 
+import chapter_6.LinkedQueue;
+import chapter_6.Queue;
+
 public class MergeSort {
 
 	public static <K> void mergeSort(K[] S, Comparator<K> comp){
@@ -30,6 +33,42 @@ public class MergeSort {
 			}
 		}
 	}
+	
+	public static <K> void mergeSort(Queue<K> S, Comparator<K> comp){
+		int n = S.size();
+		if(n<2)
+			return;
+		int mid = n/2;
+		Queue<K> S1 = new LinkedQueue<>();
+		Queue<K> S2 = new LinkedQueue<>();
+		
+		for(int i=0; i<mid; i++){
+			S1.enqueue(S.dequeue());
+		}
+		while(!S.isEmpty())
+			S2.enqueue(S.dequeue());
+		
+		mergeSort(S1, comp);
+		mergeSort(S2, comp);
+		
+		merge(S1, S2, S, comp);
+		
+	}
+	
+	public static <K> void merge(Queue<K> S1, Queue<K> S2, Queue<K> S, Comparator<K> comp){
+		while(!S1.isEmpty() && !S2.isEmpty()){
+			if(comp.compare(S1.first(), S2.first())<0)
+				S.enqueue(S1.dequeue());
+			else
+				S.enqueue(S2.dequeue());
+		}
+		
+		while(!S1.isEmpty())
+			S.enqueue(S1.dequeue());
+		while(!S2.isEmpty())
+			S.enqueue(S2.dequeue());
+	}
+	
 	
 	public static void main(String[] args){
 		
